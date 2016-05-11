3d71b4a749eb226f18fa823434ba7e12c1569b8e
Live session user <marunoafp@gmail.com>
2016-05-11 13:48:24 +0000

    tests passed

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -13,15 +13,57 @@ var Direction = require("./Direction").Direction;
 
 function MatrixRotator(matrix){
   this.matrix = matrix;
-  
 };
 
 //                                         |-- Must be Direction.CW
 //                                         v        or Direction.CCW
+
 MatrixRotator.prototype.rotate = function(direction) {
   // do work here
+  
+  var matrixCW = [];
 
-  // must be a valid Direction, see Direction.js
+  if(direction === Direction.CW) {
+
+	  for (var i = 0; i < this.matrix.length; i++) {
+
+      matrixCW[i] = [];
+
+		  for (var j = 0; j < this.matrix.length; j++) {
+
+        matrixCW[i][j] = this.matrix[this.matrix.length - 1 - j][i]; 
+
+      } 
+
+  	}
+
+    this.matrix = matrixCW;
+
+  } else if (Direction.CCW) {
+
+  var matrixCCW = [];
+
+    for (var k = 0; k < this.matrix.length; k++) {
+
+      matrixCCW[k] = [];
+
+      for (var l = 0; l < this.matrix.length; l++) {
+
+        matrixCCW[k][l] = this.matrix[l][this.matrix.length - 1 - k];
+
+      }
+
+    }
+
+    this.matrix = matrixCCW;
+
+  }
+
+}; 
+	
+
+	// must be a valid Direction, see Direction.js
+
+  
 
 
-};
