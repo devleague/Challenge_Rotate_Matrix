fd077345693c3abc1c16d31a983d6a04a43bfc06
Brad McKinney <brad.mckin@gmail.com>
2019-01-16 10:23:25 -1000

    creates a method on the MatrixRotator class that rotates a 2D matrix based on user input

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -11,16 +11,35 @@ const Direction = require("./Direction").Direction;
  *        @returns the rotated matrix
  */
 module.exports = class MatrixRotator {
-  constructor( matrix ) {
+  constructor(matrix) {
     this.matrix = matrix;
   }
 
   //      |-- Must be Direction.CW
   //      v        or Direction.CCW
-  rotate (direction) {
-    // do work here
+  rotate(direction) {
+    let rotated = [[], [], [], [], []];
 
-    // must be a valid Direction, see Direction.js
+    if (direction === 'ClockWise') {
+      for (let i = 0; i < this.matrix.length; i++) {
+        for (let x = 0; x < this.matrix[i].length; x++) {
+          rotated[x].unshift(this.matrix[i][x]);
+        }
+      }
+      this.matrix = rotated;
+    } else if (direction === 'CounterClockWise') {
+      for (let i = 0; i < this.matrix.length; i++) {
+        let counter = 4;
 
+        for (let x = 0; x < this.matrix[i].length; x++) {
+          rotated[counter].push(this.matrix[i][x]);
+          counter--;
+        }
+      }
+      this.matrix = rotated;
+    } else {
+      throw new Error('Invalid input: Please provide a valid direction, CW (Clockwise) or CCW (CounterClockwise)');
+    }
   }
 };
+
